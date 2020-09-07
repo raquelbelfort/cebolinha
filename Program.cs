25e5a593d8bff0e4c5d6379714a79c75fcdcb9e4
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-09-07 18:45:37 -0300

    Adição de resolução.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Olá, usuário! Como será que o Cebolinha diria o famoso trava-língua 'O rato roeu a roupa do rei de Roma'? Tente escrever essa frase.");
+            string cebolinha = Console.ReadLine().Replace("r" , "l") .Replace("R", "L");
+            Console.WriteLine(cebolinha);
+        }
+    }
+}
